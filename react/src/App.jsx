40e3c4d46b9f480099724e7cbc4200aa64e5e36c
jagrat1005 <agrawaljagrat46@gmail.com>
2025-03-20 07:50:41 +0530

    Initial commit

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Homepage from './pages/Homepage';
+import loginpage from './pages/loginpage';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
